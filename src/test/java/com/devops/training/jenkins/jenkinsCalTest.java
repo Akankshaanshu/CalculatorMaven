d411c3d8e6394c76f6ffd84bbcceb886ada7efd7
Akankshaanshu <89064929+Akankshaanshu@users.noreply.github.com>
2021-10-15 22:48:19 +0530

    Update jenkinsCalTest.java

--- src/test/java/com/devops/training/jenkins/jenkinsCalTest.java
+++ src/test/java/com/devops/training/jenkins/jenkinsCalTest.java
@@ -10,12 +10,12 @@ public class jenkinsCalTest {
 
 	@Test
 	public void addtest() {
-		jenkinsCalculator mycalc=new jenkinsCalculator();
+		JenkinsCalculator mycalc=new JenkinsCalculator();
 		assertEquals(10,mycalc.addNumbers(5, 5));
 	}
 	@Test
 	public void subtracttest() {
-		jenkinsCalculator mycalc=new jenkinsCalculator();
+		JenkinsCalculator mycalc=new JenkinsCalculator();
 		assertEquals(5,mycalc.subtractNumbers(10, 5));
 	}
 }
